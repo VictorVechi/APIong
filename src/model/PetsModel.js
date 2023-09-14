867ed5396bddd698748c1ecc118194bfb777d200
VictorVechi <victorhvechi@gmail.com>
2023-09-14 19:18:48 -0300

    Alteração no tipo de entrada do id_unidade

--- src/model/PetsModel.js
+++ src/model/PetsModel.js
@@ -1,12 +1,11 @@
 import mongoose from "mongoose";
 
 const Pets = mongoose.model("Pets", {
-    id_pet: Number,
     nome: String,
     raca: String,
     peso: Number,
     idade: String,
-    id_unidade: Number
+    id_unidade: String
 })
 
 export default Pets;
