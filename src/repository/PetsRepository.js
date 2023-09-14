730f6a64c86b7754a53da280ec1c9b4866a6158a
VictorVechi <victorhvechi@gmail.com>
2023-09-14 19:20:45 -0300

    implementa a manipulação da collection de pets

---
+++ src/repository/PetsRepository.js
@@ -0,0 +1,30 @@
+import Repository from "./Repository.js"
+import Pets from "../model/PetsModel.js"
+
+class PetsRepository extends Repository{
+
+
+    static async criarPet (data){
+        await this.create(Pets, data)
+    }
+
+    static async buscarPets(){
+        const response = await this.findAll(Pets)
+        return response
+    }
+
+    static async buscarPetPorId(id){
+        const response = await this.findById(Pets, id)
+        return response
+    }
+
+    static async atualizarPet(id, data){
+        await this.updateById(Pets, id, data)
+    }
+   
+    static async deletarPet(id){
+        await this.deleteById(Pets, id)
+    }
+}
+
+export default PetsRepository
