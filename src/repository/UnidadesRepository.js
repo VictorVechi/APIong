f8ed75f94f540739de1ef8ec8b2c2316089f35fa
VictorVechi <victorhvechi@gmail.com>
2023-09-15 20:16:43 -0300

    Métodos de manipulação da collection Unidades

---
+++ src/repository/UnidadesRepository.js
@@ -0,0 +1,28 @@
+import Repository from "./Repository.js"
+import Unidades from "../model/UnidadesModel.js"
+
+class UnidadesRepository extends Repository {
+    
+    static async criarUnidades(data){
+        await this.create(Unidades, data)
+    }
+
+    static async buscarUnidades(){
+        const response = await this.findAll(Unidades)
+        return response
+    }
+
+    static async buscarUnidadesPorId(id){
+        const response = await this.findById(Usuarios, id)
+        return response
+    }
+
+    static async atualizarUnidades(id, data){
+        await this.updateById(Unidades, id, data)
+    }
+
+    static async deletarUnidades(id){
+        await this.deleteById(Unidades, id)
+    }
+
+} export default UnidadesRepository
