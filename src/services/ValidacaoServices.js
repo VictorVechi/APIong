4a2dc452cc58eb4fcb94919039dd3530ec1e04c7
VictorVechi <victorhvechi@gmail.com>
2023-09-17 09:56:55 -0300

    feat: :sparkles: Cria o código com validações de campos

--- src/services/ValidacaoServices.js
+++ src/services/ValidacaoServices.js
@@ -1,15 +1,18 @@
 import Repository from "../repository/Repository.js"
 
 class ValidacaoServices {
-    static async exists(MongooseModel, id){
 
+    static async exists(MongooseModel, id){
         try{
             await Repository.findById(MongooseModel, id)
             return true
         }catch(error){
             return false 
         }
-        
-        
     }
+
+    static validarNome(nome){
+        return nome.length > 2 && isNaN(nome)
+    }
+
 } export default ValidacaoServices
