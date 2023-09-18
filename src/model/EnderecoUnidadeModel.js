83e165bcdbbf3336c6fb6ae8d01bfe49dbdf913e
JojoPrc <joseproenca2005@gmail.com>
2023-09-17 22:41:07 -0300

    revisando Model de EnderecoUnidade

--- src/model/EnderecoUnidadeModel.js
+++ src/model/EnderecoUnidadeModel.js
@@ -1,6 +1,6 @@
 import mongoose from "mongoose";
 
-const EnderecoUnidade = mongoose.model("Endereco_Unidade",{
+const EnderecoUnidade = mongoose.model("EnderecoUnidade",{
     cep: String,
     rua: String,
     numero: String
