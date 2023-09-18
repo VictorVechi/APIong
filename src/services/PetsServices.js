37a6996c1101e0c5fa053a795692a884b95f918d
VictorVechi <victorhvechi@gmail.com>
2023-09-17 22:17:49 -0300

    remoção de importação desnecessária

--- src/services/PetsServices.js
+++ src/services/PetsServices.js
@@ -1,6 +1,5 @@
 import Pets from "../model/PetsModel.js"
 import ValidacaoServices from "./ValidacaoServices.js"
-import Unidades from "../model/UnidadesModel.js"
 class PetsServices extends ValidacaoServices {
     static async validarBusca(id) {
         const response = await this.exists(Pets, id)
